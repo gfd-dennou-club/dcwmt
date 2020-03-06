f6cf452ec9a89432fa108440fd4138f554acaec2
matsumura <matsumura@matsumura.local>
2020-03-06 15:13:53 +0900

    ユーザの変更する箇所を明示

--- define_tile.js
+++ define_tile.js
@@ -1,5 +1,15 @@
-//タイル定義ファイル
-var pt = L.gridLayer.numDataGroup('../../GridLayer/dataTile/PT',{
+var xxxx = L.gridLayer.numDataGroup('xxxx',{
+  tileSize : new L.Point(xxxx, xxxx)//タイルの大きさを定義 new L.Point(横の解像度, 縦の解像度),
+});
+var xxxx = L.gridLayer.vectorNumData('xxxx','xxxx',{
+  tileSize : new L.Point(xxxx,xxxx)
+});
+var baseMaps    = {"xxxx": xxxx, ....};
+var overlayMaps = {"xxxx": xxxx, ....};
+//以下は書き換えない
+var layergroup = L.layerCtl(baseMaps, overlayMaps);
+layergroup.addTo(map);
+/*var pt = L.gridLayer.numDataGroup('../PT',{
   tileSize : new L.Point(240, 240)//タイルの大きさを定義 new L.Point(横の解像度, 縦の解像度),
 });
 var vec_uv = L.gridLayer.vectorNumData('../../GridLayer/dataTile/U/','../../GridLayer/dataTile/V/',{
@@ -11,4 +21,4 @@ var vec_uv = L.gridLayer.vectorNumData('../../GridLayer/dataTile/U/','../../Grid
 var baseMaps    = {"PT": pt, "U-V": vec_uv};
 var overlayMaps = {"PT": pt, "U-V": vec_uv};
 var layergroup = L.layerCtl(baseMaps, overlayMaps);
-layergroup.addTo(map);
+layergroup.addTo(map);*/
