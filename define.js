c2f1ae35b888b4464946f66ef40aa5bd6c288062
Daichi-1637 <Daichi@daichi.local>
2020-08-10 21:11:19 +0900

    define.jsのvarをconstに変更

--- define.js
+++ define.js
@@ -1,11 +1,11 @@
-var scale_x=[0,18200];
-var scale_y=[0,18200];
-var dir_root = "../tile"
-var value_name=["PT"];
-var value_name_vec=[];
-var dir_time=["t=14.5"];
-var dir_dim=["h=6000m"];
-var tile_size_x = 240;
-var tile_size_y = 240;
-var max_zoom = 4;
-var continuous = true
+const scale_x=[0,18200];
+const scale_y=[0,18200];
+const dir_root = "../tile"
+const value_name=["PT"];
+const value_name_vec=[];
+const dir_time=["t=14.5"];
+const dir_dim=["h=6000m"];
+const tile_size_x = 240;
+const tile_size_y = 240;
+const max_zoom = 4;
+const continuous = true
