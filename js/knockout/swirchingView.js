02ea952fb4fb4daab5aba8fef2edba259b38c762
Daichi-1637 <Daichi@daichi.local>
2021-07-01 11:08:59 +0900

    各ライブラリの切り替えおよび表示

--- js/knockout/swirchingView.js
+++ js/knockout/swirchingView.js
@@ -318,6 +318,93 @@ window.onload = function(){
                     // mapにレイヤを追加
                     scalar_layer.addTo(view);
                     break;
+                    case "OpenLayers":
+                        map.setAttribute("style", "height: 500px; width: 100%;");
+                        const olMap = new ol.Map({
+                            target: map,
+                            layers: [
+                                new ol.layer.Tile({
+                                    source: new ol.source.XYZ({
+                                        url: `${DEFINE.ROOT}/Ps/time=32112/{z}/{x}/{y}.png`,
+                                        tileLoadFunction: (imageTile, src) => {
+                                            const canvas = document.createElement("canvas");
+                                            [canvas.width, canvas.height] = [256, 256];
+                                            const img = new Image();
+                                            const onload = new Promise((resolve, reject) => {
+                                                img.src = src;
+                                                resolve(img)
+                                            })
+                                            
+                                            onload.then(
+                                                value => {
+                                                    const redraw = new Promise((resolve, reject) => {
+                                                        const context = canvas.getContext('2d');
+                                                        context.drawImage(value, 0, 0);
+                                                        const img = context.getImageData(0, 0, canvas.width, canvas.height);
+                                                        const rgba = img.data;
+                                                        let min, max;
+                                                        let red, green, blue;
+                                                        let dataView = new DataView(new ArrayBuffer(32));
+                                                        let scalarData = new Array();
+
+                                                        for(let i = 0; i < canvas.height * canvas.width; i++){
+                                                            const bias_rgb_index = i*4;
+                                                            red =   rgba[bias_rgb_index    ]  << 24;
+                                                            green = rgba[bias_rgb_index + 1]  << 16;
+                                                            blue =  rgba[bias_rgb_index + 2]  << 8;
+                                                        
+                                                            dataView.setUint32(0, red + green + blue);
+                                                            scalarData[i] = dataView.getFloat32(0);
+                                                        
+                                                            if(i === 0)
+                                                                min = max = scalarData[i];
+                                                            else{
+                                                                if(min > scalarData[i]) min = scalarData[i];
+                                                                if(max < scalarData[i]) max = scalarData[i];
+                                                            }
+                                                        }
+                                                    
+                                                        for(let i = 0; i < canvas.height * canvas.width; i++){
+                                                            const bias_rgb_index = i*4;
+                                                            const colormap_per_scalardata = clrmap_04.length / (max - min);
+                                                            const colormap_index = parseInt(colormap_per_scalardata * (scalarData[i] - min));
+                                                            let rgb;
+                                                        
+                                                            if(scalarData[i] === 0.0000000000)          { rgb = {r:255, g:255, b:255}; }
+                                                            else if(clrmap_04.length <= colormap_index) { rgb = clrmap_04[clrmap_04.length - 1]; }
+                                                            else if(0 >= colormap_index)                { rgb = clrmap_04[0]; }
+                                                            else                                        { rgb = clrmap_04[colormap_index]; } 
+                                                        
+                                                            img.data[bias_rgb_index  ] = rgb.r;
+                                                            img.data[bias_rgb_index+1] = rgb.g;
+                                                            img.data[bias_rgb_index+2] = rgb.b;
+                                                            img.data[bias_rgb_index+3] = 255;
+                                                        }
+
+                                                        context.putImageData(img, 0, 0);
+
+                                                        resolve(canvas);
+                                                    })
+
+                                                    redraw.then(
+                                                        canvas => {
+                                                            imageTile.getImage().src = canvas.toDataURL("image/png");
+                                                        },
+                                                        reason => {console.error(reason)}
+                                                    );
+                                                },
+                                                reason => { console.error(reason); }
+                                            )
+                                        }
+                                    })
+                                })
+                            ],
+                            view: new ol.View({
+                                center: [-472202, 7530279],
+                                zoom: 2,
+                            })
+                        })
+                        break;
             }
             return true;
         }   
