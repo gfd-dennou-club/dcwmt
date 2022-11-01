a5edc3a5bc81cfdb7e2b5a74d0c9518216100b0f
Daichi-1637 <Daichi@daichi.local>
2022-11-01 13:07:35 +0900

    revise define.js

--- define.js
+++ define.js
@@ -17,7 +17,7 @@ const DEFINE = {
 	VECTOR: [
 		{
 			NAME: ["U", "V"],
-			FIXED: ["time=32112.0"],	// 固定する次元（ 名前=値 ）		
+			FIXED: ["time=32112.0/sig=0.9987513422966003"],	// 固定する次元（ 名前=値 ）		
 			SIZE: {X: 256, Y: 256},		// 各タイルのサイズ（ ピクセル数 ）		
 			MAXIMUMLEVEL: 2,			// 拡大率		
 			AXIS: {X: "lon", Y: "lat"}, // タイルの縦横の軸の名前		
